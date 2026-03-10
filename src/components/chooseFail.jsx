d1b3285346f632a10fd0ea8a0de88b9a20e07321
Kenta-Tanaka-820 <kevinluchuan29@gmail.com>
2026-03-11 01:13:07 +0900

    不要ファイル整理

--- src/components/chooseFail.jsx
+++
@@ -1,4 +0,0 @@
-export const ChooseFail = () => {
-    return ( <input type="file" accept='.jpg'/> );
-};
-
--- src/components/onCapture.jsx
+++
@@ -1,4 +0,0 @@
-export const onCapture = (e) => {
-     const file = e.target.files?.[0];
-    if (!file) return;
-};
